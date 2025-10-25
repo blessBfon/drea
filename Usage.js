75bdec1ede7af616366a9078b458eddb09a71e2f
Drax_ <167656105+blessBfon@users.noreply.github.com>
2025-10-25 15:12:39 +0100

    Usage

--- Usage.js
+++ Usage.js
@@ -3,7 +3,7 @@ import {normalizer,
     validateMany,
     validateEntry,
     ClassicModel,
-CustomClassicModel} from './drea.js'
+    CustomClassicModel} from './drea.js'
 
 //Passing a regex as the rule
 console.log(validateEntry({
