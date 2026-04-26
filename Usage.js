6c36f2a1ba6ccc5647069c14dee262fe841f6724
Drax_ <blessfonmtoh@gmail.com>
2026-04-27 00:07:11 +0100

    feat: v3.0.0 release with new schema features and breaking changes

--- Usage.js
+++
@@ -1,8 +0,0 @@
-import { CustomClassicModel, None } from "./drea.js";
-
-const schema = {
-  name: { rule: None, errorMsg: "Name must be a string" },
-  age: { rule: (v) => typeof v === "number", errorMsg: "Age must be a number" }
-};
-const custom = new CustomClassicModel(schema);
-console.log(custom.validate({ name: "JoeBless", age: 25 }));
