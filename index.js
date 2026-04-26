6a1f19d5525db79e0fc31823aa64ef010e96b4d6
Drax_ <blessfonmtoh@gmail.com>
2026-04-27 00:32:30 +0100

    chore(core): update package entry point

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+export { 
+      validateEntry,
+      isUsernameValid,
+      isEmailValid,
+      isPhoneNumberValid,
+      isPasswordValid,
+      isRequired,
+      Normalizer,
+      validateMany,
+      ClassicModel,
+      CustomClassicModel,
+      URL,None
+
+} from './drea.js';
+
+
