08f7142b8e3afef263faa4f91101dd883c38897c
Drax_ <blessfonmtoh@gmail.com>
2026-07-22 09:03:51 +0100

    feat: changed favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,8 +3,8 @@ export default{
     title:"drea",
     description:"A RuleForge validation system for JavaScript and Typescript.",
     head:[
-        ['link',{ rel:'icon', href:'/favicon-6.ico'}],
-        ['link',{ rel:'shortcut icon', href:'/favicon-6.ico'}]
+        ['link',{ rel:'icon', href:'/favicon-7.ico'}],
+        ['link',{ rel:'shortcut icon', href:'/img/drea-logo-2.png'}]
     ],
     themeConfig:{
         nav:[
